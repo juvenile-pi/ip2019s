3b3e2f7a271991077d2b553a74a9a51ea25a30ff
juvenile-pi <52521637+juvenile-pi@users.noreply.github.com>
2019-07-05 17:37:14 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,10 +4,39 @@
 
 
 function start(e) {
-
+    var name = [
+{
+        char:'楊',
+        cns:'1-644E',
+        unicode:'694A',
+        big5:'B7A8'
+    },
+    {
+        char:'智',
+        cns:'1-5F614',
+        unicode:'667A',
+        big5:'B4BC'
+    },
+    {
+        char:'宇',
+        cns:'1-4775',
+        unicode:'5B87',
+        big5:'A674'
+    }
+];
+    var ele = document.getElementById("div1");
+    for (var i = 0; i<3 ; i++) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += '    cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += '    unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '    big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += '<br>';
+        }
 }
-
-/*
+        /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 
 document.addEventListener(event, function, useCapture)
