a1d97a12ad2d4b42eff1986f193ab883b0e73a10
juvenile-pi <52521637+juvenile-pi@users.noreply.github.com>
2019-07-09 15:23:32 +0800

    Add files via upload

---
+++ portfolio/labs/lab04/lab.js
@@ -0,0 +1,19 @@
+$(function(){
+    var card =['ace-of-club.svg' , 'ace-of-diamonds.svg' , 'ace-of-hearts.svg' , 'ace-of-spades.svg'];
+
+for (var i = 0 ; i <16 ; i++){
+    var number = Math.floor((Math.random()*4));
+    
+    $('#d2').append('<img  class="issue" src="./' + card[number] +  '">');
+    }
+
+    $('#b1').on('click',function(){
+        $(".issue").hide();
+        for (var i = 0; i < 16; i++){
+            var r = Math.floor(Math.random()*4);
+
+            $('#d2').append('<img src="./joker-card.svg">');
+        }
+    });
+
+});
